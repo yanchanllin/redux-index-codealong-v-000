974b6337f152780ac53bc69b4674808dd32ec73d
Yan C Lin <yonalin1@gmail.com>
2019-05-30 20:17:08 +0000

    Automatically backed up by Learn

---
+++ src/components/todos/TodosContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+ 
+class TodosContainer extends Component {
+ 
+  render() {
+    return(
+      <div></div>
+    );
+  }
+};
+ 
+export default connect()(TodosContainer);
