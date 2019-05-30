44d4f19d8559351dd3ea39ae2935a096697bbd71
Yan C Lin <yonalin1@gmail.com>
2019-05-30 20:17:46 +0000

    Automatically backed up by Learn

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -10,4 +10,10 @@ class TodosContainer extends Component {
   }
 };
 
-export default connect()(TodosContainer);
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  }
+}
+ 
+export default connect(mapStateToProps)(TodosContainer);
